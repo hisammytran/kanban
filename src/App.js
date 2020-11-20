41f3ef8845e4b251ed2d49d9e058961ba5ff1217
unknown <pst001@ucsd.edu>
2020-11-19 19:40:23 -0800

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+class Task extends React.Component{
+  render(){
+    return(
+      <li>
+        {this.props.value}
+      </li>
+    )
+  }
+}
+
+class TodoItem extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      list: [],
+      task: "",
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    let currList = this.state.list;
+    currList.push(this.state.task);
+    this.setState({list: currList});
+    alert(this.state.list);
+    this.setState({task: ""});
+    // alert(currList);
+    // alert(this.state.task);
+    // currList.push(this.state.task);
+    // this.setState({list: currList});
+    // alert(this.state.list[0])
+
+  }
+  handleChange(event){
+    this.setState({task: event.target.value})
+  }
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" placeholder="Task Name" value = {this.state.task} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value = "Submit"/>
+        <ul>
+          {this.state.list.map((item,i)=>
+            <li key={i}>{item}</li>
+          )}
+        </ul>
+      </form>
+      
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p>Test Text</p>
+        <TodoItem name="sam"></TodoItem>
       </header>
     </div>
-  );
+  ); 
 }
+
 
 export default App;
