40d54fa93dd1347f5b6aa398da987d61d566563a
unknown <pst001@ucsd.edu>
2020-11-20 19:03:51 -0800

    removed logic and started page formatting

--- src/App.js
+++ src/App.js
@@ -1,72 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-class Task extends React.Component{
-  render(){
-    return(
-      <li>
-        {this.props.value}
-      </li>
+// class TodoItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       list: [],
+//       task: "",
+//     }
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     let currList = this.state.list;
+//     currList.push(this.state.task);
+//     this.setState({ list: currList });
+//     this.setState({ task: "" });
+//   }
+//   handleChange(event) {
+//     this.setState({ task: event.target.value })
+//     return (
+//       <p>hi</p>
+//     )
+//   }
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           <input type="text" placeholder="Task Name" value={this.state.task} onChange={this.handleChange} />
+//         </label>
+//         <input type="submit" value="Submit" />
+//         <ul>
+//           {this.state.list.map((task, key) =>
+//             <li key={key}>{task}<button>Start</button></li>
+//           )}
+//         </ul>
+//       </form>
+
+//     )
+//   }
+// }
+
+class Queues extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <table>
+          <tr>
+            <th className='c1'>Queue</th>
+            <th className='c2'>In Progress</th>
+            <th className='c3'>Completed</th>
+          </tr>
+          <tbody></tbody>
+        </table>
+      </div>
     )
   }
 }
 
-class TodoItem extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      list: [],
-      task: "",
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+class NewTask extends React.Component {
 
-  handleSubmit(event){
-    event.preventDefault();
-    let currList = this.state.list;
-    currList.push(this.state.task);
-    this.setState({list: currList});
-    alert(this.state.list);
-    this.setState({task: ""});
-    // alert(currList);
-    // alert(this.state.task);
-    // currList.push(this.state.task);
-    // this.setState({list: currList});
-    // alert(this.state.list[0])
-
-  }
-  handleChange(event){
-    this.setState({task: event.target.value})
-  }
-  render(){
-    return(
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <input type="text" placeholder="Task Name" value = {this.state.task} onChange={this.handleChange}/>
-        </label>
-        <input type="submit" value = "Submit"/>
-        <ul>
-          {this.state.list.map((item,i)=>
-            <li key={i}>{item}</li>
-          )}
-        </ul>
-      </form>
-      
+  render() {
+    return (
+      <div>
+        <button>New Task</button>
+      </div>
     )
   }
 }
+
+class Kanban extends React.Component {
+  render() {
+    return (
+      <div className="content">
+        Kanban
+        <NewTask />
+        <Queues />
+      </div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>Test Text</p>
-        <TodoItem name="sam"></TodoItem>
-      </header>
+      <Kanban />
     </div>
-  ); 
+  );
 }
 
 
